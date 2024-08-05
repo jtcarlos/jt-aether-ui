b97c4a5c6c150018058a1c21a8dcd08cd2cafa60
Thom Carlos <john.thomas.carlos0913@gmail.com>
2024-08-05 17:06:37 +0800

    updated Button component size and styling

--- src/interface/Button/button.config.ts
+++ src/interface/Button/button.config.ts
@@ -1,16 +1,18 @@
 export const buttonSize = {
-  xs: "text-xs",
-  sm: "text-sm",
-  md: "text-base",
-  lg: "text-lg",
+  xs: "text-sm px-2 py-1",
+  sm: "text-sm px-2 py-1.5",
+  md: "text-base px-3 py-2",
+  lg: "text-base px-3.5 py-2.5",
 }
 
 export const buttonVariant = {
+  default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 border-transparent",
   primary: "bg-primary border-primary text-white hover:bg-primary-hover",
   secondary: "bg-secondary border-secondary text-white hover:bg-secondary-hover",
   danger: "bg-danger border-danger text-white hover:bg-danger-hover",
   success: "bg-success border-success text-white hover:bg-success-hover",
   warning: "bg-warning border-warning text-white hover:bg-warning-hover",
-  outlined: "bg-transparent border-primary text-primary hover:bg-primary-hover hover:text-white",
+  outlined:
+    "bg-transparent border-primary text-primary hover:bg-primary-hover hover:text-white ring-1 ring-inset ring-primary",
   text: "bg-transparent border-none text-primary hover:bg-primary-hover-text",
 }
--- src/interface/Button/index.tsx
+++ src/interface/Button/index.tsx
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCircleNotch, IconDefinition } from "@fortawesome/free-solid-svg-icons"
 
 type ButtonSize = "xs" | "sm" | "md" | "lg"
-type ButtonVariant = "primary" | "secondary" | "danger" | "success" | "warning" | "outlined" | "text"
+type ButtonVariant = "default" | "primary" | "secondary" | "danger" | "success" | "warning" | "outlined" | "text"
 
 type ButtonIcon = { left?: IconDefinition; right?: IconDefinition }
 
@@ -50,7 +50,7 @@ const Button: React.FC<ButtonProps> = ({ children, text, size = "md", variant = 
       className={classNames(
         textSize,
         buttonColor,
-        "px-3 py-2 h-fit w-fit border rounded-md transition-colors disabled:opacity-75",
+        "rounded-md font-semibold shadow-sm transition-colors disabled:opacity-75",
         props?.className
       )}
     >
