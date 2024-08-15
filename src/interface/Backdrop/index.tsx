2df1fab1f025330951fb1fec61e6b1b18841a933
Thom Carlos <john.thomas.carlos0913@gmail.com>
2024-08-15 19:23:28 +0800

    updated circle attribute

--- src/interface/Backdrop/index.tsx
+++ src/interface/Backdrop/index.tsx
@@ -28,7 +28,7 @@ const Backdrop: React.FC<BackdropProps> = ({ color = "white", height = "h-12", w
           viewBox="0 0 24 24"
           fill="none"
         >
-          <circle className="opacity-25" cx="12" cy="12" r="10" stroke={color || "black"} stroke-width="4" />
+          <circle className="opacity-25" cx="12" cy="12" r="10" stroke={color || "black"} strokeWidth="4" />
           <path
             d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
             fill={color || "black"}
